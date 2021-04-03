fae56c9b313d6dce17a4011b728144644d577594
Lev Butkovskiy <>
2021-04-03 20:56:22 +0500

    Clear project, NavBar, Internationalisation

---
+++ client/src/UI/NavBar/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useTranslation } from "react-i18next";
+import './index.css';
+
+import { Dropdown } from 'semantic-ui-react'
+
+import logo from '../../img/logo.svg';
+import UKFlag from '../../img/UKFlag.svg';
+import RUFlag from '../../img/RUFlag.svg';
+
+
+function NavBar(props) {
+    const [t, i18n] = useTranslation();
+
+    const langs = [
+        {
+            key: 'en',
+            value: 'en',
+            image: { avatar: true, src: UKFlag },
+        },
+        {
+            key: 'ru',
+            value: 'ru',
+            image: { avatar: true, src: RUFlag },
+        },
+    ]
+
+    const handleChangeLanguage = (event, data) => {
+        i18n.changeLanguage(data.value);
+    }
+
+    return (
+        <div className="navBar">
+            <img className="logo" src={logo} alt="logo"/>
+            <div className="title">
+                <span>
+                    <span className="bold">MobiCash</span>
+                    &nbsp;
+                    <span className="light">Zen</span>
+                </span>
+            </div>
+            <Dropdown 
+                onChange={handleChangeLanguage}
+                className="langDropdown"
+                inline
+                options={langs}
+                defaultValue={langs[0].value}
+            />
+        </div>
+    );
+}
+
+export default NavBar;
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>

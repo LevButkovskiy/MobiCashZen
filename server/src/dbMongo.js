6a2cbb98e201d161c6f55b922b09bfdac3b7a342
Lev Butkovskiy <>
2021-04-03 19:23:42 +0500

    initial commit server

---
+++ server/src/dbMongo.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+var readLine = require("readline");
+const DB_URL = "mongodb://localhost/mobicash-zen";
+
+if (process.platform === "win32"){
+    var rl = readLine.createInterface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on ("SIGINT", function (){
+        process.emit ("SIGINT");
+    });
+}
+
+// Подключение к mongodb
+mongoose.connect(DB_URL, {
+    useUnifiedTopology: true,  // установка опций
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+mongoose.connection.on('connected', function(){
+    console.log('Mongoose connect to ' + DB_URL);
+});
+
+mongoose.connection.on('error', function(err){
+    console.log('Mongoose connect error ' + err);
+});
+
+mongoose.connection.on('disconnected', function(){
+    console.log('Mongoose disconnected');
+});
+
+// Перехват события завершения процесса
+var gracefulShutdown = function(msg,callback){
+    mongoose.connection.close(() => {
+        console.log("Mongoose disconnect through " + msg);
+    }
+)};
+
+process.once('SIGUSR2',function(){
+    gracefulShutdown('nodemon restart', function(){
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT',()=>{
+    gracefulShutdown('app termination', function(){
+        process.exit(0);
+    });
+});
